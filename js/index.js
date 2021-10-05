6788bf652715c4789ef359268157aab3ca6754d6
IlhamSetiawan <ilhamsetiawan2000@gmail.com>
2021-10-06 00:27:29 +0700

    fix push

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const peserta = {
+    "Ilham adi": {
+        "link": "pages/wawan/index.html",
+        "photo": "img/wawan.jpg"
+    },
+
+
+    "namamu": {
+
+        "photo": "img/fotomu.jpg"
+    },
+
+};
